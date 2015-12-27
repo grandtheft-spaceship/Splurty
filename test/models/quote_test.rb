aeb9d653fe04dc63a4efeac3e3c43d4d36d27271
grandtheft-spaceship <justdizon71@gmail.com>
2015-12-27 14:29:55 +0000

    Created a test for the new feature to display author's ID with each quote

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   
+   test "unique_tag" do
+   	quote = Quote.create(:author => 'Slim Shady', :saying => 'I came to save these new generations of babies//From parents who failed to raise them \'cause they\'re lazy')
+   	expected = 'SS#' + quote.id.to_s
+   	actual = quote.unique_tag
+   	assert_equal expected, actual
    end
+
 end
